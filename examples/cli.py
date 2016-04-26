40b0026c9e6ce8363fbd3f5ad17784f1b6c02d01
luckydonald <luckydonald@flutterb.at>
2016-04-26 15:44:39 +0200

    Improved output of basic CLI. Still without commands yet.

--- examples/cli.py
+++ examples/cli.py
@@ -1,32 +1,29 @@
 # -*- coding: utf-8 -*-
+import json
+from pytgbot import Bot
 from luckydonaldUtils.logger import logging
+from luckydonaldUtils.interactions import input, answer
+
+try:
+    from somewhere import API_KEY  # so I don't upload them to github :D
+except ImportError:
+    API_KEY = None
+# end if
 
 __author__ = 'luckydonald'
+
 logger = logging.getLogger(__name__)
 
 
-
-# -*- coding: utf-8 -*-
-
-__author__ = 'luckydonald'
-
-import logging
-logger = logging.getLogger(__name__)
-
-from pytgbot import Bot, InputFile
-
-from somewhere import API_KEY  # so I don't upload them to github :D
-# Just remove the line, and add API_KEY="..." and TEST_CHAT = 12345
-
-from queue import Queue
 
 from threading import Thread
 
 # get you bot instance.
-from luckydonaldUtils.interactions import input
 
 
-def main():
+def main(API_KEY):
+    if API_KEY is None:
+        API_KEY = answer("Input API key")
     bot = Bot(API_KEY)
     my_info=bot.get_me()
     print("Information about myself: {info}".format(info=my_info))
@@ -36,48 +33,75 @@ def main():
     notice = "You can enter commands now.\n"
     while True:
         cmd = input(notice)
-        notice = ""
-        print("would do something with {cmd}".format(cmd=repr(cmd)))  # TODO.
+        notice = ""  # never display again.
+        print("Would do something with {cmd}".format(cmd=repr(cmd)))  # TODO.
+        try:
+            command, args = cmd.split(" ", maxsplit=1)
+            # print(bot.do(command, **json.loads(args)))
+        except Exception as e:
+            print(e)
     # end while
 # end def
 
 
 def get_updates(bot):
-    last_update_id = 0  # something.
-
+    last_update_id = 0
     while True:  # loop forever.
         for update in bot.get_updates(limit=100, offset=last_update_id+1)["result"]:  # for every new update
             last_update_id = update["update_id"]
-            print(update_to_string(update))
+            try:
+                print(update_to_string(update))
+            except AttributeError:
+                print(update)
+                raise
+            # end try
+        # end for
+    # end while
+# end def
 
 
 def update_to_string(data):
-    {'message': {'text': 'asd', 'from': {'username': 'luckydonald', 'id': 10717954, 'first_name': 'Luckydonald'},
-                 'chat': {'username': 'luckydonald', 'id': 10717954, 'type': 'private', 'first_name': 'Luckydonald'},
-                 'message_id': 5, 'date': 1461674481}, 'update_id': 307711634}
-
-    {'message': {'from': {'username': 'luckydonald', 'id': 10717954, 'first_name': 'Luckydonald'},
-                 'new_chat_member': {'username': 'test4458bot', 'id': 133378542, 'first_name': 'Derpibot'},
-                 'message_id': 6, 'date': 1461674709, 'chat': {'id': -142938350, 'type': 'group', 'title': 'derpytest'},
-                 'new_chat_participant': {'username': 'test4458bot', 'id': 133378542, 'first_name': 'Derpibot'}},
-     'update_id': 307711635}
-
-
-    if data.message.chat.type == 'private':
-        return "{message_id} {from.first_name}: {text}".format(**data.message)
-    elif data.message.chat.type == 'group':
-        return "{message_id} {from.first_name} ({chat.title}): {text}".format(**data.message)
+    if "message" in data:
+        msg = data.message
+        msg["update_id"] = data.update_id
+        msg["from"]["print"] = peer_to_string(msg["from"])
+        if msg.chat.type == 'private':
+            if "text" in msg:
+                return "[msg {message_id}] {from.print}: {text}".format(**msg)
+        elif msg.chat.type == 'group':
+            if "text" in msg:
+                return "[msg {message_id}] {from.print} ({chat.title}): {text}".format(**msg)
+    elif "inline_query" in data:
+        qry = data.inline_query
+        qry["update_id"] = data.update_id
+        qry["from"]["print"] = peer_to_string(qry["from"])
+        return "[query {id}] {from.print}: {query}".format(**qry)
+    # end if message
     return str(data)
 # end def
 
-"""
-[*Revision 60: first steps kinect connect*](https://sep.isf.cs.tu-bs.de/redmine/projects/16-cg-2/repository/revisions/60
-) — _Kay-Kristian Donner_
-Die Fenstergröße wird zum Programmstart einmalig gesetzt, und kann/soll anschließend nur zwischen dieser Größe und de...
-"""
+
+def peer_to_string(peer):
+    if "title" in peer:  # chats
+        return "{title}".format(**peer)
+    if "first_name" in peer:
+        if "last_name" in peer:
+            return "{first_name} {last_name}".format(**peer)
+        # end if last_name
+        return "{first_name}".format(**peer)
+    # not first_name
+    elif "last_name" in peer:
+        return "{last_name}".format(**peer)
+    elif "username" in peer:
+        return "@{username}".format(**peer)
+    elif "id" in peer:
+        return "#{id}"
+    # end if
+    return "<UNKNOWN>"
+# end def
 
 if __name__ == '__main__':
-    main()
+    main(API_KEY)
 # end if
 
 # end file
