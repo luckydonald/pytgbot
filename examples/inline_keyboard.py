0d51a1a862fef33d40f6f0c92206af3b4d5ea106
luckydonald <luckydonald@flutterb.at>
2016-06-28 15:42:50 +0200

    Updated to answer the callbacks.

--- examples/inline_keyboard.py
+++ examples/inline_keyboard.py
@@ -30,6 +30,7 @@ def main():
                 last_update_id = update.update_id
                 if not (update.message and "/start" in update.message.text) and not update.callback_query:
                     continue
+
                 if update.message:
                     print("got message: {msg}".format(msg=update.message))
                     origin, peer_id = get_sender_infos(update.message)
@@ -46,7 +47,7 @@ def main():
                         reply_markup=markup
                     )
                     print(result)
-                else:
+                else if update.callback_query:
                     # callback_query.message is the original message the bot sent
                     peer_id, current_image, do_submit = update.callback_query.data.split(";")
                     peer_id, current_image = int(peer_id), int(current_image)  # str -> int
@@ -55,8 +56,10 @@ def main():
                     result_image, markup = generate_page(current_image, peer_id, photos)
                     assert isinstance(result_image, PhotoSize)
                     if do_submit:
+                        bot.answer_callback_query(update.callback_query.id, text="Sending...")
                         result = bot.send_photo(chat_id=update.callback_query.message.chat.id, photo=result_image.file_id)
                     else:
+                        bot.answer_callback_query(update.callback_query.id, text=None)
                         result = bot.edit_message_text(
                             "Profile pic {num}\n{w}x{h}, {size}B".format(
                                 num=current_image, w=result_image.width, h=result_image.height, size=result_image.file_size
