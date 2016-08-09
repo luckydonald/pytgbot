0258f150341998df0a2dbe07f03bf37bfc70ec85
luckydonald <luckydonald@flutterb.at>
2016-08-09 13:42:47 +0200

    [HOTFIX] InputFileFromURL. Hopefully now working correctly...

--- pytgbot/api_types/sendable/__init__.py
+++ pytgbot/api_types/sendable/__init__.py
@@ -48,10 +48,40 @@ class InputFileFromDisk(InputFile):
 class InputFileFromURL(InputFile):
     def __init__(self, file_url, file_name=None, file_mime=None):
         super(InputFileFromURL, self).__init__(None, file_name, file_mime)
+        self.file_url = file_url
         self.file = requests.get(file_url)
         self.file_name = file_name if file_name else None
         self.file_mime = file_mime if file_mime else None
+        self.url_to_name_and_mime()
     # end def __init__
+
+    def __str__(self):
+        file_size = len(self.file.content)
+        return (
+            "{clazz_name!s}("
+             "file_url={self.file_url!r}, file_name={self.file_name!r}, file_mime={self.file_mime!r}, "
+            "file_size={file_size}"
+        ).format(clazz_name=self.__class__.__name__, file_size=file_size, self=self)
+    # end def __str__
+
+    def url_to_name_and_mime(self):
+        if not self.file_name:
+            import os  # http://stackoverflow.com/a/18727481/3423324
+            try:  # python 2:
+                from urlparse import urlparse
+            except ImportError:  # python 3:
+                from urllib.parse import urlparse
+            # end try
+            path_part = urlparse(self.file_url).path
+            self.file_name = os.path.basename(path_part)
+        # end if
+        if not self.file_mime:
+            import magic  # pip install python-magic
+            self.file_mime = magic.from_buffer(requests.get(self.file_url).content, mime=True)
+        # end if
+    # end def
+
+
 
     def get_request_files(self, var_name):
         return {var_name: (self.file_name, self.file.content, self.file_mime)}
--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["DictObject", "requests", "requests[security]"]
+    install_requires=["DictObject", "requests", "requests[security]", "python-magic"]
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
