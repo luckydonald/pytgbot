4eaf5520c8715547380517dbbb475a7859ad4d2d
luckydonald <luckydonald@flutterb.at>
2016-11-24 22:20:27 +0100

    Bump version: 2.3.1 → 2.3.2

--- pytgbot/__init__.py
+++ pytgbot/__init__.py
@@ -3,11 +3,11 @@ import logging
 from .bot import Bot
 
 __author__ = 'luckydonald'
-__version__ = "2.3"
+__version__ = "2.3.2"
 __all__ = ["api_types", "Bot"]
 VERSION = __version__
 
-API_2.3.1
+API_VERSION = "2" + "." + "3"
 API_DATE = "November 21, 2016"
 
 logger = logging.getLogger(__name__)
