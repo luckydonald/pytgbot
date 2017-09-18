1c8d30e56000b94fd472e4c1f21c882be9a75c8e
luckydonald <luckydonald@flutterb.at>
2017-09-19 01:28:05 +0200

    Version Bump 3.1.0 => 3.3.0

--- pytgbot/__init__.py
+++ pytgbot/__init__.py
@@ -3,12 +3,12 @@ import logging
 from .bot import Bot
 
 __author__ = 'luckydonald'
-__version__ = "3.1.0"
+__version__ = "3.3.0"
 __all__ = ["api_types", "Bot"]
 VERSION = __version__
 
-API_VERSION = "3" + "." + "1"  # so the bumpversion script doesn't breaks this.
-API_DATE = "June 30, 2017"
+API_VERSION = "3" + "." + "3"  # so the bumpversion script doesn't breaks this.
+API_DATE = "August 23, 2017"
 
 logger = logging.getLogger(__name__)
 
