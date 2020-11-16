1327b1262aa5167b2bf2ccf370c0200395ecd701
luckydonald <mac2019.git@luckydonald.de>
2020-11-16 16:45:44 +0100

    API v5.0: Merged changes (8): Added missing file to version control.

---
+++ pytgbot/api_types/__init__.pyi
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from luckydonaldUtils.exceptions import assert_type_or_raise
+from luckydonaldUtils.encoding import unicode_type, to_unicode as u
+from typing import Any, Union, List
+__author__ = 'luckydonald'
+
+
+class TgBotApiObject(object):
+    """
+    Base class for every api object class.
+
+    Optional keyword parameters:
+    
+    :param _raw: Optional. Original data this object was generated from. Could be `None`.
+    :type  _raw: None | dict
+    """
+# end class TgBotApiObject
