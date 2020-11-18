634538f7d8b04970f4bbfda12298af50273369df
luckydonald <mac2019.git@luckydonald.de>
2020-11-18 02:01:29 +0100

    Fixed importing the wrong bot.

--- pytgbot/bot/__init__.py
+++ pytgbot/bot/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from luckydonaldUtils.logger import logging
-from .asynchronous import Bot
+from .synchronous import SyncBot as Bot
 
 __author__ = 'luckydonald'
 __all__ = ["Bot", "asynchronous", "synchronous"]
