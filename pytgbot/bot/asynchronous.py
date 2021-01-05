287b205f98f55c1be69ae6959befb56cfb99e3e2
luckydonald <mac2019.git@luckydonald.de>
2021-01-05 03:13:08 +0100

    [code_generation] Regenerated only the client files, as those had no whitespaces before.

--- pytgbot/bot/asynchronous.py
+++ pytgbot/bot/asynchronous.py
@@ -547,7 +547,7 @@ class AsyncBot(BotBase):
         :param chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
         :type  chat_id: int | str|unicode
 
-        :param photo: Photo to send. Pass a file_id as String to send a photo that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a photo from the Internet, or upload a new photo using multipart/form-data. More info on Sending Files »
+        :param photo: Photo to send. Pass a file_id as String to send a photo that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a photo from the Internet, or upload a new photo using multipart/form-data. The photo must be at most 10 MB in size. The photo's width and height must not exceed 10000 in total. Width and height ratio must be at most 20. More info on Sending Files »
         :type  photo: pytgbot.api_types.sendable.files.InputFile | str|unicode
 
 
