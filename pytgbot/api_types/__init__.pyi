5b84687d24ed1c20ac248fd828163ed562ea2a3c
luckydonald <mac2019.git@luckydonald.de>
2021-01-05 03:43:09 +0100

    Added type hints for a few functions.

--- pytgbot/api_types/__init__.pyi
+++ pytgbot/api_types/__init__.pyi
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from luckydonaldUtils.exceptions import assert_type_or_raise
 from luckydonaldUtils.encoding import unicode_type, to_unicode as u
-from typing import Any, Union, List
+from luckydonaldUtils.typing import JSONType
+from typing import TypeVar, Type, Union
+
 __author__ = 'luckydonald'
 
 
@@ -10,8 +12,14 @@ class TgBotApiObject(object):
     Base class for every api object class.
 
     Optional keyword parameters:
-    
+
     :param _raw: Optional. Original data this object was generated from. Could be `None`.
     :type  _raw: None | dict
     """
 # end class TgBotApiObject
+
+
+REQUIRED_TYPE = TypeVar('REQUIRED_TYPE')
+def from_array_list(required_type: Type[REQUIRED_TYPE], result: JSONType, list_level: int, is_builtin: bool) -> REQUIRED_TYPE: pass
+
+def as_array(obj: Union[TgBotApiObject, list, tuple, dict, JSONType]) -> JSONType: pass
