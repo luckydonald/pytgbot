574d35431c9bdbd0bf2556dedeeeea2961554349
luckydonald <mac2019.git@luckydonald.de>
2021-03-29 22:59:27 +0200

    [code_generation] Okey, so we have special code, but now we have a patch for it.

---
+++ code_generation/pytgbot-generator4.patch.sh
@@ -0,0 +1,11 @@
+
+cd code_generation/output/pytgbot/
+git apply ../../pytgbot-generator4.patch
+
+#  cp api_types/__init__.py ../../../pytgbot/api_types/__init__.py
+find . -maxdepth 5 \( ! -type d \) -exec sh -c 'echo cp  "$@" "../../../pytgbot/$@"' _ {} \;
+# non-dry-run:
+find . -maxdepth 5 \( ! -type d \) -exec sh -c 'cp  "$@" "../../../pytgbot/$@"' _ {} \;
+#  cp ./api_types/__init__.py ../../../pytgbot/./api_types/__init__.py
+
+git apply -R ../../pytgbot-generator4.patch
