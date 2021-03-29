705c2bb724b61f9b07f6ed1ce2191ea0e998ecc4
luckydonald <mac2019.git@luckydonald.de>
2021-03-29 19:42:52 +0200

    [test] Test the InputMediaVideo with positional args only.

--- tests/api_types/sendable/test_input_media.py
+++ tests/api_types/sendable/test_input_media.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_InputMediaVideo_thumb_files(self):
         vid1 = 'https://derpicdn.net/img/view/2016/12/21/1322277.mp4'
         pic1 = 'https://derpicdn.net/img/2017/7/21/1491832/thumb.jpeg'
-        i = InputMediaVideo(InputFileFromURL(vid1), thumb=InputFileFromURL(pic1))
+        i = InputMediaVideo(InputFileFromURL(vid1), InputFileFromURL(pic1))
         d = i.get_request_data('lel', full_data=True)
         self.assertEqual(d[0], {'type': 'video', 'thumb': 'attach://lel_thumb', 'media': 'attach://lel_media'})
         self.assertListEqual(list(d[1].keys()), ['lel_media', 'lel_thumb'])
